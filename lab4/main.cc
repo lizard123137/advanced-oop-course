f9952055207475bbb1e3c3ee9f85614ad59469c2
lizard123137 <lizard123137@gmail.com>
2024-12-16 18:27:04 +0100

    finished 4.4

--- lab4/main.cc
+++ lab4/main.cc
@@ -1,7 +1,28 @@
 #include <iostream>
 #include <algorithm>
+#include <list>
 #include <random>
 #include <deque>
+
+class func_obj {
+public:
+    bool operator()(int x, int y) {
+        int dig_sum_x = 0;
+        int dig_sum_y = 0;
+
+        while (x) {
+            dig_sum_x += x % 10;
+            x /= 10;
+        }
+
+        while (y) {
+            dig_sum_y += y % 10;
+            y /= 10;
+        }
+
+        return dig_sum_x > dig_sum_y;
+    }
+};
 
 int main(void) {
     // 4.2
@@ -39,6 +60,28 @@ int main(void) {
     std::sort(posegregowane.begin(), posegregowane.end(), obj());
 
     std::for_each(posegregowane.begin(), posegregowane.end(), [](int x) { std::cout << x << ", "; });
+    std::cout << std::endl;
+
+    // 4.4
+    std::vector<int> to_be_sorted_vec = { 0, 500, 3, 80, 1, 700, 2, 4, 6000, 9 };
+    std::list<int> to_be_sorted_list = { 0, 55, 3, 8, 100, 7, 2, 4, 6, 99 };
+
+    bool increasing = false;
+    auto sort_func = [increasing](int x, int y) {
+        return (increasing) ? ((int)log10(x) > (int)log10(y)) : ((int)log10(y) <= (int)log10(x));
+    };
+
+    std::sort(to_be_sorted_vec.begin(), to_be_sorted_vec.end(), sort_func);
+    to_be_sorted_list.sort(func_obj());
+
+    for (auto& x : to_be_sorted_vec) {
+        std::cout << x << ", ";
+    }
+    std::cout << std::endl;
+
+    for (auto& x : to_be_sorted_list) {
+        std::cout << x << ", ";
+    }
     std::cout << std::endl;
 
     return EXIT_SUCCESS;
--- lab6/main.cc
+++ lab6/main.cc
@@ -1,26 +1,40 @@
 #include <iostream>
-#include <boost/multi_index_member.hpp>
+
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/ordered_index.hpp>
+#include <boost/multi_index/identity.hpp>
+#include <boost/multi_index/member.hpp>
+
+using namespace boost::multi_index;
 
 struct Student {
 	std::string index;
 	std::string imie;
 	std::string nazwisko;
-	std::strign plec;
+	std::string plec;
 };
 
-typedef multi_index_container<Student, indexed_by<
-// Multiset dla naw i set dla index
-ordered_non_unique<member<Student, std::string, &Student::imie>,
-ordered_unique<member<Student, std::string, &Student::index>,
-// multiset nazw i unordered mutiset dla pl
-ordered_non_unique<> // TODO finish implementation
-ordered_unique<>
+typedef multi_index_container<
+	Student,
+	indexed_by<
+		ordered_non_unique<member<Student, std::string, &Student::nazwisko>>,
+		ordered_unique<member<Student, std::string, &Student::index>>,
+		ordered_unique<member<Student, std::string, &Student::plec>>
+	>
 > student_multi;
+
+typedef student_multi::nth_index<0>::type nazwisko_idx;
+typedef student_multi::nth_index<1>::type index_idx;
+typedef student_multi::nth_index<2>::type plec_idx;
 
 int main() {
 	student_multi students;
 
-	students.inser({"	
+	students.insert({"99532", "Adam", "Nowak", "M"});
+	students.insert({"99533", "Adam", "Kowalski", "M"});
+	students.insert({"99534", "Karolina", "Nowak", "K"});
+	students.insert({"99535", "Karolina", "Piotrkowska", "K"});
+	students.insert({"99536", "Anna", "Grudzien", "K"});
 
 	return EXIT_SUCCESS;
 }
