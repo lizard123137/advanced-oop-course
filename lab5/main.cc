dba0e28a0fcbc3974424e2f34d369bbfbbb829b8
lizard123137 <lizard123137@gmail.com>
2024-12-25 01:02:12 +0100

    finished exercise 5.2 and worked on lab 6

--- lab5/main.cc
+++ lab5/main.cc
@@ -1,17 +1,17 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <set>
+#include <unordered_set>
 #include <utility>
 
 // 5.1
 std::pair<int, int> getRepeat(std::vector<int> v) {
-	std::set<int> dict {};
+	std::unordered_set<int> dict {};
 
 	int repeating = -1;
 	int sum = 0;
 
 	for(auto& el : v) {
-		std::cout << "Current element is: " << el << std::endl;
 		if (!dict.insert(el).second)
 			repeating = el;
 		else
@@ -21,15 +21,59 @@ std::pair<int, int> getRepeat(std::vector<int> v) {
 	return std::make_pair(repeating, sum - repeating);
 }
 
+// 5.2
+// The whole enable_if part is ment to only allow types that can be added
+template<typename T>
+typename std::enable_if<std::is_arithmetic<T>::value, T>::type
+add_unique(std::unordered_set<T> a, std::unordered_set<T> b) {
+    /**
+     * What we are trying to do from a mathematical point of view
+     * is finding the symmetric difference of two sets.
+     * The c++ library algorithms has a function for that.
+     * 
+     * After that we just sum the resulting elements.
+     */
+    std::vector<T> sym_diff;
+
+    std::set_symmetric_difference(
+        a.begin(), a.end(),
+        b.begin(), b.end(),
+        std::back_inserter(sym_diff)
+    );
+
+    /**
+     * We cannot initialize this, since we do not know what type it is.
+     * C++ however is supposed to zero initialize variables, so let's hope it does its job...
+     */
+    T acc;
+
+    for (auto &x : sym_diff) {
+        acc += x;
+    }
+
+    return acc;
+}
+
 int main() {
 	// 5.1
 	std::vector<int> test = { 5, 3, 2, 1, 1, 6 };
 
 	auto res = getRepeat(test);
-	std::cout << "Repeating element: " 
+	std::cout << "5.1\n" 
+        << "Repeating element: " 
 		<< res.first << "\n"
 		<< "Sum of unique: "
-		<< res.second << std::endl;
+		<< res.second
+        << std::endl;
+
+    // 5.2
+    std::unordered_set<int> a = { 0, 2, 4, 6, 8 };
+    std::unordered_set<int> b = { 1, 2, 4, 6, 8 };
+
+    std::cout << "5.2\n"
+        << "The sum of unique elements is: "
+        << add_unique(a, b)
+        << std::endl;
 
 	return EXIT_SUCCESS;
 }
--- lab6/main.cc
+++ lab6/main.cc
@@ -23,18 +23,40 @@ typedef multi_index_container<
 	>
 > student_multi;
 
-typedef student_multi::nth_index<0>::type nazwisko_idx;
-typedef student_multi::nth_index<1>::type index_idx;
-typedef student_multi::nth_index<2>::type plec_idx;
+typedef student_multi::nth_index<0>::type nazwisko_type;
+typedef student_multi::nth_index<1>::type index_type;
+typedef student_multi::nth_index<2>::type plec_type;
 
 int main() {
 	student_multi students;
 
-	students.insert({"99532", "Adam", "Nowak", "M"});
-	students.insert({"99533", "Adam", "Kowalski", "M"});
-	students.insert({"99534", "Karolina", "Nowak", "K"});
-	students.insert({"99535", "Karolina", "Piotrkowska", "K"});
-	students.insert({"99536", "Anna", "Grudzien", "K"});
+	students.insert({"99535", "Adam", "Nowak", "M"});
+	students.insert({"99212", "Adam", "Kowalski", "M"});
+	students.insert({"99523", "Karolina", "Nowak", "K"});
+	students.insert({"99755", "Karolina", "Piotrkowska", "K"});
+	students.insert({"99333", "Anna", "Grudzien", "K"});
+
+    // Odwolanie domyslnym indexem (pierwszym zdefiniowanym)
+    std::cout << "Liczba studentow o nazwisku Nowak: "
+        << students.count("Nowak")
+        << std::endl;
+
+    // Odwolania za pomoca pozostalych indeksow
+    index_type &index_idx = students.get<1>();
+    plec_type &plec_idx = students.get<2>();
+
+    auto student = index_idx.find("99535");
+
+    std::cout << "Student o numerze albumu 99535 to: "
+        << student->imie << " "
+        << student->nazwisko << " "
+        << student->plec
+        << std::endl;
+
+    // Index typu ordered_unique grupuje rekordy o tej samej wartosci
+    std::cout << "Liczba plci wsrod studentow: "
+        << plec_idx.size()
+        << std::endl;
 
 	return EXIT_SUCCESS;
 }
