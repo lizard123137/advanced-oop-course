ae0aea24d05c5aa2964e908b545a737275a68c9d
lizard123137 <lizard123137@gmail.com>
2025-01-24 21:13:36 +0100

    finished 10.2 assignment

--- lab10/main.cc
+++ lab10/main.cc
@@ -7,7 +7,7 @@ int main() {
     
     std::cout << "Podaj wyrazenie w ktorym wyszukane zostana liczby zmiennoprzecinkowe: ";
     std::string haystack;
-    std::cin >> haystack;
+    std::getline(std::cin, haystack);
     
     auto begin = std::sregex_iterator(haystack.begin(), haystack.end(), re_float);
     auto end = std::sregex_iterator();
@@ -15,6 +15,19 @@ int main() {
     for(std::sregex_iterator i = begin; i != end; ++i) {
         std::smatch match = *i;
         std::cout << "Liczba zmiennoprzecinkowa: " << match.str() << std::endl;
+    }
+
+    // 10.2
+    std::regex re_hour(R"(([01][0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?)");
+
+    std::cout << "Podaj wyrazenie w ktorym wyszukana zostanie godzina: ";
+    std::getline(std::cin, haystack);
+
+    begin = std::sregex_iterator(haystack.begin(), haystack.end(), re_hour);
+
+    for(std::sregex_iterator i = begin; i != end; ++i) {
+        std::smatch match = *i;
+        std::cout << "Godzina: " << match.str() << std::endl;
     }
 
     return EXIT_SUCCESS;
